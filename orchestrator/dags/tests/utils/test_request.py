9fe6bee8930708c15e1da96996bd2a0bddd911ef
Victor-P-Martins <vmartins.eng@hotmail.com>
2024-01-16 00:10:51 -0300

    feat: add Request class and tests

---
+++ orchestrator/dags/tests/utils/test_request.py
@@ -0,0 +1,64 @@
+from orchestrator.dags.utils import Request
+
+
+def test_request_get_successful():
+    """
+    Test case to verify a successful GET request using the Request class.
+
+    It sends a GET request to the specified URL with the given headers and
+    checks if the response is not None and if it is successful (status code 200).
+
+    """
+    url = "https://example.com"
+    headers = {"Content-Type": "application/json"}
+    request = Request(headers=headers)
+
+    response = request.get(url)
+
+    assert response is not None
+    assert response.ok
+
+
+def test_request_get_failed():
+    """
+    Test case for the 'get' method of the Request class when the URL is invalid.
+    """
+    url = 123
+    request = Request()
+
+    response = request.get(url)
+
+    assert response is None
+
+
+def test_request_post_successful():
+    """
+    Test the successful POST request using the Request class.
+
+    This function tests the functionality of the `post` method of the Request class.
+    It sends a POST request to the specified URL with the provided data and checks
+    if the response is not None and if it is successful (status code 200).
+
+    """
+    url = "https://example.com"
+    data = {"key": "value"}
+    request = Request()
+
+    response = request.post(url, data=data)
+
+    assert response is not None
+    assert response.ok
+
+
+def test_request_post_error():
+    """
+    Test case for the 'post' method of the Request class when an error occurs.
+
+    This test verifies that the 'post' method returns None when an invalid URL is provided.
+    """
+    url = 123
+    request = Request()
+
+    response = request.post(url)
+
+    assert response is None
---
+++ orchestrator/dags/utils/request.py
@@ -0,0 +1,89 @@
+import traceback
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+
+class Request:
+    """
+    A class for making HTTP requests with retry functionality.
+
+    Args:
+        headers (dict): Optional. Headers to be included in the requests.
+
+    Attributes:
+        headers (dict): Headers to be included in the requests.
+        session (requests.Session): Session object for making requests.
+    """
+
+    def __init__(self, headers: dict = None):
+        retries = Retry(total=5, backoff_factor=2)
+        self.headers = headers
+        self.session = requests.Session()
+        self.session.mount("https://", HTTPAdapter(max_retries=retries))
+
+    def __verify_response(self, response: object) -> object:
+        """
+        Verifies the response of the request and handles errors.
+
+        Args:
+            response (object): The response object returned by the request.
+
+        Returns:
+            object: The response object if it is successful, otherwise None.
+        """
+
+        if response.ok:
+            return response
+
+        print(f"Erro na requisição: {response}")
+        return response
+
+    def get(self, url: str, payload: dict = None) -> object:
+        """
+        Sends a GET request to the specified URL.
+
+        Args:
+            url (str): The URL to send the request to.
+            payload: Optional. The payload to include in the request.
+
+        Returns:
+            object: The response object if the request is successful, otherwise None.
+        """
+        try:
+            response = self.session.request(
+                "GET", url, headers=self.headers, params=payload
+            )
+
+        except requests.exceptions.RequestException:
+            print(f"{traceback.format_exc()}")
+            return None
+
+        return self.__verify_response(response)
+
+    def post(self, url: str, data: dict = None, json: dict = None) -> object:
+        """
+        Sends a POST request to the specified URL.
+
+        Args:
+            url (str): The URL to send the request to.
+            data: Optional. The data to include in the request.
+            json: Optional. The JSON payload to include in the request.
+
+        Returns:
+            object: The response object if the request is successful, otherwise None.
+        """
+        try:
+            response = self.session.request(
+                "POST", url, headers=self.headers, data=data, json=json
+            )
+
+        except requests.exceptions.RequestException:
+            print(f"{traceback.format_exc()}")
+            return None
+
+        return self.__verify_response(response)
+
+
+if __name__ == "__main__":
+    request = Request()
+    request.get("https://www.google.com")
